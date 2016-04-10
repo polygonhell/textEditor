f7a9136e5cae8d3a05eca0be1edc6e210b01b030
Rob <polygonhell@gmail.com>
2016-04-10 12:59:38 -0700

    Working cursor motion

--- src/Main.hs
+++ src/Main.hs
@@ -1,15 +1,44 @@
 module Main where
 
 import Data.Sequence
+import Debug.Trace
+import Control.Concurrent
 
 import TextBuffer
 import TTYRender
+import View
 
 
 multiLineContent :: BufferContent
 multiLineContent = fromList ["This is a test", "Dogs and Cats Living Together", "Purple rain falling", "Complete drivel", "And another line to act as a test", "short line"]
 
-multiLineBuffer = Buffer multiLineContent (Cursor 0 0 0)
+multiLineBuffer = Buffer multiLineContent (Cursor 0 5 2)
+initialViewState = ViewState 0 0 100 10
+
+
+updateBuffer:: String -> Buffer -> Buffer
+updateBuffer k b = b' where
+   b' = case k of 
+    "Up" -> cursorUp b
+    "Down" -> cursorDown b
+    "Left" -> cursorLeft b
+    "Right" -> cursorRight b
+    _ -> b
+
+
+
+loop :: Buffer -> ViewState -> IO()
+loop b v = do 
+  -- print b
+  key <- readKeys
+  let b' = updateBuffer key b
+  draw v b'
+  loop b' v
+
 
 main :: IO ()
-main = draw 0 0 8 3 multiLineBuffer
+main = do
+  initTTY
+  -- draw initialViewState multiLineBuffer
+  -- threadDelay (10*1000000)
+  loop multiLineBuffer initialViewState
--- src/TTYRender.hs
+++ src/TTYRender.hs
@@ -1,13 +1,17 @@
 {-# LANGUAGE RecordWildCards #-}
 module TTYRender where
 
-import Data.Sequence as S
+import Control.Concurrent
 import Data.Foldable
 import Data.List as L
-import Control.Concurrent
-import TextBuffer
+import Data.Sequence as S
+import System.IO
+import System.Posix.IO (fdRead, stdInput, stdOutput)
+import System.Posix.Terminal
 import Text.Printf
-import Control.Monad.Loops
+
+import TextBuffer
+import View
 
 
 drawLines :: Int -> Int -> BufferContent -> IO ()
@@ -40,21 +44,52 @@ setTopAndBottom = printf "%c[%d;%dr" (toEnum 27 :: Char)
 resetTopAndBottom :: String
 resetTopAndBottom = printf "%c[r" (toEnum 27 :: Char) 
 
-draw :: Int -> Int -> Int -> Int -> Buffer -> IO ()
-draw topRow leftCol width height  b@Buffer{..} = do 
 
-  let cls = printf "%c[2J" (toEnum 27 :: Char) ::String
-      buffSlice = S.take height $ S.drop topRow content
+readKeys :: IO String
+readKeys = do
+  (str, bytes) <- fdRead stdInput 3 
+  return $ case str of
+    "\ESC[A" -> "Up"
+    "\ESC[B" -> "Down"
+    "\ESC[C" -> "Right"
+    "\ESC[D" -> "Left"
+    _ -> "ow"
+
+
+initTTY :: IO ()
+initTTY = do 
+  oldTermSettings <- getTerminalAttributes stdInput
+  {- modify settings -}
+  let newTermSettings = flip withoutMode  EnableEcho   . -- don't echo keystrokes
+                        flip withoutMode  ProcessInput $ -- turn on non-canonical mode
+                        oldTermSettings
+    -- flip withTime     vtime        . -- wait at most vtime decisecs per read
+    -- flip withMinInput vmin         $ -- wait for >= vmin bytes per read
+
+  setTerminalAttributes stdInput newTermSettings Immediately
+
+  -- application `finally` setTerminalAttributes stdInput oldTermSettings Immediately
+  
+  putStrLn "Inited" 
+
+draw :: ViewState -> Buffer -> IO ()
+draw ViewState{..} b@Buffer{..} = do 
+
+  let buffSlice = S.take height $ S.drop top content
   putStr cls
   -- putStr $ setTopAndBottom 0 5
   putStr resetTopAndBottom
   putStr $ toPos 0 0
-  drawLines leftCol width buffSlice 
+  drawLines left width buffSlice 
 
   -- putStr $ toPos 0 0
   -- putStr (concat $ L.replicate 3 revIndex )
-  putStr $ toPos 2 5
-  threadDelay $ 10 * 1000000
+  let cursorX = col cursor - left + 1
+      cursorY = line cursor - top + 1
+  putStr $ toPos cursorY cursorX
+  -- putStr $ toPos 0 0
+  hFlush stdout
+  -- threadDelay $ 10 * 1000000
 
 
 
