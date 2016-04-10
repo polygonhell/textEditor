37dd754808ddb5f4711d6f05df1dca88e399093c
Rob <polygonhell@gmail.com>
2016-04-10 10:11:32 -0700

    messing with ansi terminal codes

--- src/TTYRender.hs
+++ src/TTYRender.hs
@@ -4,8 +4,10 @@ module TTYRender where
 import Data.Sequence as S
 import Data.Foldable
 import Data.List as L
+import Control.Concurrent
 import TextBuffer
 import Text.Printf
+import Control.Monad.Loops
 
 
 drawLines :: Int -> Int -> BufferContent -> IO ()
@@ -23,11 +25,11 @@ cls = printf "%c[2J" (toEnum 27 :: Char)
 toPos :: Int -> Int -> String
 toPos = printf "%c[%d;%dH" (toEnum 27 :: Char)
 
-scrollUp :: String
-scrollUp = printf "%cD" (toEnum 27 :: Char)
+index :: String
+index = printf "%cD" (toEnum 27 :: Char)
 
-scrollDown :: String
-scrollDown = printf "%cM" (toEnum 27 :: Char)
+revIndex :: String
+revIndex = printf "%cM" (toEnum 27 :: Char)
 
 smoothScroll :: String
 smoothScroll = printf "%c[?4h" (toEnum 27 :: Char)
@@ -35,12 +37,25 @@ smoothScroll = printf "%c[?4h" (toEnum 27 :: Char)
 setTopAndBottom :: Int -> Int -> String
 setTopAndBottom = printf "%c[%d;%dr" (toEnum 27 :: Char) 
 
+resetTopAndBottom :: String
+resetTopAndBottom = printf "%c[r" (toEnum 27 :: Char) 
+
 draw :: Int -> Int -> Int -> Int -> Buffer -> IO ()
 draw topRow leftCol width height  b@Buffer{..} = do 
 
   let cls = printf "%c[2J" (toEnum 27 :: Char) ::String
       buffSlice = S.take height $ S.drop topRow content
   putStr cls
-  putStr $ setTopAndBottom 0 10
+  -- putStr $ setTopAndBottom 0 5
+  putStr resetTopAndBottom
   putStr $ toPos 0 0
   drawLines leftCol width buffSlice 
+
+  -- putStr $ toPos 0 0
+  -- putStr (concat $ L.replicate 3 revIndex )
+  putStr $ toPos 2 5
+  threadDelay $ 10 * 1000000
+
+
+
+
