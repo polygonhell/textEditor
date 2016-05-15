bc910653580a37f6c6f885ac94a53336120f253e
Rob Povey <polygonhell@gmail.com>
2016-05-15 14:57:27 -0700

    Messing with ways to represent the Tiled windows

--- src/TextEditor.hs
+++ src/TextEditor.hs
@@ -1,64 +1,63 @@
-{-# LANGUAGE RecordWildCards #-}
+-- {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE ExistentialQuantification #-}
+
 
 module TextEditor where 
 
 
-import qualified System.Console.Terminal.Size  as TS
-import qualified Data.Sequence as S
+-- import qualified System.Console.Terminal.Size  as TS
+-- import qualified Data.Sequence as S
 import qualified Data.Map as M
+-- import Data.Traversable
 import TextBuffer
 
 
 data BufferManager = BufferManager {textBuffers :: M.Map String Buffer}
 
--- This view is just the Top left offset of the Buffer, the buffer Size is dictated by the window Splits
-data View = View Int Int
-data WindowManager = AppWindow { active :: Int  -- Active Window index by left recursive walk
-                               , width :: Int 
-                               , height :: Int 
-                               , child :: WindowManager }       
-                   | VSplit { line :: Int
-                            , top :: WindowManager 
-                            , bottom :: WindowManager }
-                   | HSplit { col :: Int 
-                            , left :: WindowManager
-                            , right :: WindowManager } 
-                   | Window { buffer :: String -- Buffer by name 
-                            , view :: View }
-                   | StatusLine
-
-data AppState = AppState { appBufferManager :: BufferManager
-                         , appWindowManager :: WindowManager
-                         }
+data SplitDirection = Horizontal
 
 
-newTextEditor :: IO AppState
-newTextEditor = do
-  Just sz <- TS.size
-  let emptyBuffer = Buffer S.empty (Cursor 0 0 0) [] 0 [] False []
-      bm = BufferManager $ M.singleton "**empty" emptyBuffer
-      mainVS = VSplit ((TS.height sz) -2) (Window "**empty" (View 0 0)) StatusLine
-      wm = AppWindow 0 (TS.width sz) (TS.height sz) mainVS
-  return $ AppState bm wm
+
+data Layout a where
+  Layout :: (LayoutClass a) =>  a -> Layout a
+
+-- data Layout a = forall l. (LayoutClass l a) => Layout (l a)
+
+class LayoutClass a where
+  doLayout :: Rect -> a -> IO () 
 
 
-getActiveWindow' :: Int -> Int -> WindowManager -> (WindowManager, Int)
-getActiveWindow' i target wm@VSplit{..} = (wm', i') where 
-  (t, ti) = getActiveWindow' (i) target top
-  (b, bi) = getActiveWindow' (ti+1) target bottom
-  (wm', i') = 
-    if ti == target 
-      then (t, ti)
-      else (b, bi)  
--- Windows with no children count by index
-getActiveWindow' i target wm  = (wm, i)
 
-getActiveWindow :: AppState -> WindowManager
-getActiveWindow as@AppState{..} = wm where 
-  AppWindow {..} = appWindowManager
-  wm = fst $ getActiveWindow' 0 active child
+-- top left width height 
+data Rect  = Rect Int Int Int Int
 
-bufferByName :: String -> AppState -> Buffer
-bufferByName name as@AppState{..} = b where 
-  BufferManager {..} = appBufferManager
-  b = textBuffers M.! name
+
+
+
+data Empty = Empty
+
+data Root where
+  Root ::(LayoutClass a) => Rect -> a -> Root
+
+data HSplit where
+  HSplit :: (LayoutClass a, LayoutClass b) => Int -> a -> b -> HSplit 
+
+
+instance LayoutClass Empty where
+  doLayout _ _ = putStr "Empty"
+
+instance LayoutClass HSplit where
+  doLayout r (HSplit _ a b) = do 
+    doLayout r a
+    doLayout r b
+    putStr "Empty"
+
+
+test :: Root
+test = Root (Rect 0 0 20 20) (HSplit 5 Empty Empty)
+
+layout :: Root -> IO()
+layout (Root r lo) = doLayout r lo
